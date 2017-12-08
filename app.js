2d029000fa3a045bc4098cc1f9ec6a5c645cabe1
Krzysztof Kolacz <kolin4@gmail.com>
2017-12-08 17:08:28 +0100

    zadania js

---
+++ app.js
@@ -0,0 +1,167 @@
+class Przedmiot {
+  constructor(name,weight){
+     this.name = name
+     this.weight = weight
+  }
+  showInfo() {
+    console.log('name: ' + this.name, ' waga: ' + this.weight)
+  }
+}
+
+function createItem(n,w){
+      if ((n.length<1) || (w < 1)){
+        if((n.length<1) && (w<1)){
+            console.log("nazwa za krotka oraz niepoprawna waga")
+         } else if (n.length<1){
+           console.log('nazwa za krotka')
+         } else {
+           console.log('zla waga')
+         }
+      }else {
+
+        return new Przedmiot(n,w);
+      }
+}
+
+
+function createItems(number,minw, maxw){
+  const itemTab =[];
+  if ((number < 1) || ( minw < 1) || (minw > maxw)){
+    return
+  }
+  // generate random words;
+  function generateRandom(no){
+
+    no = no>0? no:10;
+    var text = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz";
+
+   for (var i = 0; i < no; i++)
+            text += possible.charAt(Math.floor(Math.random()* possible.length));
+            return text;
+    }
+
+  for ( let i = 0; i < number; i++){
+    let name = generateRandom(Math.floor(Math.random()*(10 - 5)+5));
+    let weight = Math.floor(Math.random()*(maxw - minw)+minw);
+    itemTab.push(new Przedmiot(name,weight))
+  }
+  return itemTab;
+}
+console.log(createItems(3,12,14))
+
+
+
+
+/////////////// plecak////////////////
+
+
+class Plecak {
+  constructor(name,weight,compression,items,size){
+    this.name = name;
+    this.weight = weight;
+    this.compression = compression;
+    this.items= items;
+    this.currentSize = this.items.length;
+    this.size = size;
+  }
+
+  add(items){
+      if ((items.length>0) && (this.currentSize < this.size)){
+        let emptySpace = this.size - this.currentSize;
+        for (let i = 0; i < items.length; i++){
+
+          if (emptySpace > 0){
+             this.currentSize += 1;
+             this.items.push(items[i]);
+              emptySpace -= 1;
+            }
+        }
+        let tempWeight = 0
+        for (let i = 0; i < this.items.length; i++){
+
+          for ( let key in this.items[i]){
+
+              if (key == 'weight'){
+                  tempWeight += this.items[i][key];
+              }
+
+          }
+
+        }
+        this.weight = tempWeight * this.compression / 100;
+      }
+      }
+
+
+  makeEmpty(){
+      this.items = [];
+      this.weight = 0;
+      this.currentSize = 0;
+  };
+
+  remove(index){
+    if (this.items[index]){
+        this.weight -= this.items[index].weight * this.compression/100;
+        this.currentSize -= 1;
+        this.items.splice(index,1)
+    }
+  }
+
+  showInfo(){
+    console.log(
+        "nazwa: " + this.name ,
+        ' Current size:' + this.currentSize,
+        ' Rozmiar plecaka: ' + this.size,
+        ' Waga plecaka:' + this.weight,
+        ' Compression ' + this.compression  );
+  }
+
+  showDetails(){
+      console.log('Nazwa plecaka ' + this.name);
+
+      for( let i = 0; i<this.items.length; i++){
+        console.log('item: ' + this.items.indexOf(this.items[i]))
+        for ( let key in this.items[i]){
+
+            console.log(key + " " + this.items[i][key])
+
+        }
+
+      }
+  }
+}
+
+function createBackpack (name, size, compression, items) {
+  let weight;
+  let itemsTemp = items ? items.slice(0,size): [];
+
+  // wyliczam wage przedmiotu
+  let allWeight = 0;
+  if (items) {
+
+      for (let i = 0; i < itemsTemp.length; i++){
+
+          for ( let key in itemsTemp[i]){
+
+              if (key == 'weight'){
+                  allWeight += itemsTemp[i][key];
+
+              }
+
+          }
+
+      }
+
+  }
+  allWeight = allWeight * compression / 100;
+  return new Plecak(name,allWeight,compression,itemsTemp,size)
+}
+
+let x =  createBackpack('adidas', 2, 100,[{name:'acb',weight:200},{name:'acb',weight:200}])
+console.log(x.showInfo())
+console.log(x.items)
+x.remove(1);
+console.log(x.showInfo())
+x.add([{name:'xxx', weight:340},{name:'xxx', weight:340}])
+console.log(x.showDetails())
